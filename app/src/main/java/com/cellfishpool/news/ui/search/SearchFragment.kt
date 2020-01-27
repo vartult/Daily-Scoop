231f04794040d88054f13f199e3f67f7cefa2ac2
Vartul Tripathi <vartult2@gmail.com>
2020-01-27 18:19:01 +0530

    Bug Fix(Loader)

--- app/src/main/java/com/cellfishpool/news/ui/search/SearchFragment.kt
+++ app/src/main/java/com/cellfishpool/news/ui/search/SearchFragment.kt
@@ -87,7 +87,6 @@ class SearchFragment : BaseFragment<SearchViewModel, SearchFragmentBinding>() {
             updateRecyclerViewAdapter(it)
         })
     }
-
     private fun showKeyboard(view: View) {
         val inputMethodManager =
             view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
