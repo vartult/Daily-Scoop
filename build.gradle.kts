e4cc9f73cc03d60a092b5900102c04e6689d27c7
Vartul Tripathi <vartult2@gmail.com>
2020-03-16 15:21:26 +0530

    Crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ buildscript {
     dependencies {
         classpath(Dependencies.gradle)
         classpath (Dependencies.kotlin_plugin)
+        classpath ("com.google.gms:google-services:4.3.3")
+        classpath ("com.google.firebase:firebase-crashlytics-gradle:2.0.0-beta02")
     }
 }
 
